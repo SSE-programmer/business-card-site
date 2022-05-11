eee87aa1f6c87ad3bead74681753e9d277713e05
Stolbennikov Stanislav <stolbennikovilmk@gmail.com>
2022-05-11 11:07:11 +0700

    Routing setup beginning.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+
+import { AppHeaderComponent} from './components/header/app.header.component';
+import { AngularResizeEventModule } from 'angular-resize-event'
+import { ClickOutsideModule } from 'ng-click-outside';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProjectsPageComponent } from './components/projects-page/projects-page.component';
+
+const appRoutes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    HomePageComponent,
+    ProjectsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularResizeEventModule,
+    ClickOutsideModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/projects-page/projects-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './projects-page.component.html',
+  styleUrls: ['./projects-page.component.scss']
+})
+export class ProjectsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
