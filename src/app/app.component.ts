f9199eb7d335a6ce7b3bb276ea70134691096fd0
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2025-01-11 14:13:45 +0700

    Deleted redundant styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { AppHeaderComponent } from './pages/app-header/app.header.component';
 import { RouterOutlet } from '@angular/router';
-import { AppFooterComponent } from './pages/app-footer/app.footer.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [
-    AppHeaderComponent,
-    RouterOutlet,
-    AppFooterComponent
-  ],
-  standalone: true
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    imports: [
+        RouterOutlet
+    ],
+    standalone: true
 })
 export class AppComponent {
-  title = 'SSE-programmer';
 }
